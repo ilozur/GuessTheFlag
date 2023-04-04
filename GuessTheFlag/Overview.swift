0f5d05a96fba90f0a1f9da46bae49c3391029b8c
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-04 20:17:50 +0300

    Using stacks to arrange views

---
+++ GuessTheFlag/Overview.swift
@@ -0,0 +1,36 @@
+//
+//  Overview.swift
+//  GuessTheFlag
+//
+//  Created by Edmond Podlegaev on 04.04.2023.
+//
+
+import SwiftUI
+
+struct Overview: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+    }
+}
+
+struct Overview_Previews: PreviewProvider {
+    static var previews: some View {
+        Overview()
+    }
+}
