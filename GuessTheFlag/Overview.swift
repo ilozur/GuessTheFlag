8d7cb9880160cf85ac6253664a2f8c977f8c0444
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-04 21:20:58 +0300

    Clear overview

--- GuessTheFlag/Overview.swift
+++ GuessTheFlag/Overview.swift
@@ -11,15 +11,8 @@ struct Overview: View {
     @State private var showingAlert = false
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Please, read this")
-        }
+        Text("Hello, world!")
+        .padding()
     }
 }
     
