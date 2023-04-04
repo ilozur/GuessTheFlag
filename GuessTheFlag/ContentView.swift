f9807472e32be2159f7e17458399c7bb802f84f6
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-04 22:45:28 +0300

    Showing the player’s score with an alert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
         ZStack {
@@ -26,13 +29,35 @@ struct ContentView: View {
                 }
                 
                 ForEach(0..<3) { number in
-                    Button { } label: {
+                    Button {
+                        flagTapped(number)
+                    } label: {
                         Image(countries[number])
                             .renderingMode(.original)
                     }
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
