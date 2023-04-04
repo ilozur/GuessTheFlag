796f7c467cfd641983cf0d1d34403d584160cb9e
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-04 21:19:59 +0300

    Showing alert messages

--- GuessTheFlag/Overview.swift
+++ GuessTheFlag/Overview.swift
@@ -8,55 +8,21 @@
 import SwiftUI
 
 struct Overview: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            Section {
-                Button("Button #1") {}
-                    .buttonStyle(.bordered)
-                Button("Button #2", role: .destructive) {}
-                    .buttonStyle(.bordered)
-                Button("Button #3") {}
-                    .buttonStyle(.borderedProminent)
-                    .tint(.mint)
-                Button("Button #4", role: .destructive) {}
-                    .buttonStyle(.borderedProminent)
-            }
-            Section {
-                Button {
-                    print("Button was tapped")
-                } label: {
-                    Text("Tap me!")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(.red)
-                }
-            }
-            Section {
-                Image(systemName: "pencil")
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Image(systemName: "pencil")
-                }
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Label("Edit", systemImage: "pencil")
-                }
-                Button {
-                    print("Edit button was tapped")
-                } label: {
-                    Label("Edit", systemImage: "pencil")
-                }
-            }
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please, read this")
         }
     }
 }
-    func executeDelete() {
-        print("New deleting")
-    }
     
-
 struct Overview_Previews: PreviewProvider {
     static var previews: some View {
         Overview()
