684d25fb380f13ba2e57d93b2e468b50121c5106
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-04-04 20:02:55 +0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Edmond Podlegaev on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
